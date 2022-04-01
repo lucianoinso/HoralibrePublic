c8476a63bd969d687a93527731f21fdb0b18414b
lucianoinso <lucianoinso@gmail.com>
2022-04-01 19:47:40 -0300

    removed old credentials data

--- horalibre_web/horalibre_web/settings.py
+++ horalibre_web/horalibre_web/settings.py
@@ -22,7 +22,7 @@ PYTHONIOENCODING="UTF-8"
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2-yp#7$u8kaq2)m-82b6tkedsetr0og@%d+xv)s$+m0ufmd%5o'
+SECRET_KEY = 'YOURSECRETKEYGOESHERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,9 +86,9 @@ WSGI_APPLICATION = 'horalibre_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'horalibre_db_11479',
-        'USER': 'hl_admin',
-        'PASSWORD': 'h@r$l1br3@119f4',
+        'NAME': 'dbname',
+        'USER': 'user',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
     }
@@ -144,9 +144,9 @@ LOGS_DIR = os.path.join(BASE_DIR, "logs")
 
 # EMAIL STUFF
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'horalibrefundacion@gmail.com'
-EMAIL_HOST_PASSWORD = 'UTr@v&tQV8^*ZRF2'
+EMAIL_HOST = 'yoursmtpserver'
+EMAIL_HOST_USER = 'youremail@company.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
 
 # DATE_INPUT_FORMATS += (
